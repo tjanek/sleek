9457214be1ee2d3ca6257383862e0d000a07ff49
Tomasz Janek <janek.tomasz@gmail.com>
2015-01-17 08:02:59 +0100

    Use @SpringBootApplication annotation

--- src/main/java/pl/tjanek/sleek/SleekApplication.java
+++ src/main/java/pl/tjanek/sleek/SleekApplication.java
@@ -2,13 +2,9 @@ package pl.tjanek.sleek;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+@SpringBootApplication
 public class SleekApplication {
 
     public static void main(String[] args) {
