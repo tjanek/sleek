62605d117476d3b25980b343f7133c9d41247e86
Tomasz Janek <janek.tomasz@gmail.com>
2015-02-26 05:19:06 +0100

    More Post repository specifications

--- src/test/groovy/pl.tjanek.sleek.post/PostRepositorySpec.groovy
+++ src/test/groovy/pl.tjanek.sleek.post/PostRepositorySpec.groovy
@@ -17,4 +17,16 @@ class PostRepositorySpec extends Specification {
         postRepository.getAllPosts().size() == 2
     }
 
+    def "should return valid post names and dates"() {
+        when:
+        def allPosts = postRepository.getAllPosts()
+
+        then:
+        allPosts[0].getAt() == "2015-02-16"
+        allPosts[0].getName() == "first post"
+        allPosts[0].getLink() == "2015-02-16-first-post"
+        allPosts[1].getAt() == "2015-02-17"
+        allPosts[1].getName() == "second post"
+        allPosts[1].getLink() == "2015-02-17-second-post"
+    }
 }
